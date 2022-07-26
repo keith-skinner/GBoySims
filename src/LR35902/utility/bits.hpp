5f92325b68ca7cc937bc14c5a180f9bebf155dfc
Keith Skinner <kskinns.dev@gmail.com>
2022-07-26 15:20:53 -0700

    added feature test macro to byteswap

--- src/LR35902/utility/bits.hpp
+++ src/LR35902/utility/bits.hpp
@@ -31,22 +31,56 @@ template<> struct bytes_to_uint<7U> { using value = uint64_t; };
 template<> struct bytes_to_uint<8U> { using value = uint64_t; };
 template<size_t V> using bytes_to_uint_t = typename bytes_to_uint<V>::value;
 
-constexpr auto byteswap(std::unsigned_integral auto v) noexcept ->decltype(v)
+#ifdef __cpp_lib_byteswap
+using std::byteswap;
+#else //__cpp_lib_byteswap
+template<std::integral T>
+constexpr T byteswap(T value) noexcept
 {
-    uint8_t * result = reinterpret_cast<uint8_t *>(&v);
-    for (size_t i = 0; i<sizeof(result)/2; ++i)
-        std::swap(result[i], result[sizeof(result)-1-i]);
-    return v;
+    static_assert(std::has_unique_object_representations_v<T>,
+                  "T may not have padding bits");
+    auto value_representation = std::bit_cast<std::array<std::byte, sizeof(T)>>(value);
+    std::ranges::reverse(value_representation);
+    return std::bit_cast<T>(value_representation);
 }
+#endif // __cpp_lib_byteswap
 
-bool is_native_endian(std::endian v)
+constexpr bool is_native_endian(std::endian v)
 {
     return std::endian::native == v;
 }
 
-constexpr auto to_little_endian(std::unsigned_integral auto v) -> decltype(v)
+constexpr auto to_little_endian(const std::unsigned_integral auto v) -> decltype(v)
 {
     if (!is_native_endian(std::endian::little))
         return byteswap(v);
     return v;
+}
+
+constexpr auto to_native_endian(const std::unsigned_integral auto v) ->decltype(v)
+{
+    return to_little_endian(v);
+}
+
+constexpr bool carry(std::unsigned_integral auto a, std::unsigned_integral auto b)
+{
+    static_assert(std::is_same_v<decltype(a), decltype(b)>, "a and b need to be the same type.");
+    using T = decltype(a);
+    constexpr T all_bits = std::numeric_limits<T>::max();
+    constexpr T all_but_last_bit = (all_bits >> 1);
+    constexpr T last_bit = all_bits ^ all_but_last_bit;
+    constexpr bool alb = a & last_bit;
+    constexpr bool blb = b & last_bit;
+    constexpr bool ablb = (a + b) & last_bit;
+    return (alb && blb && !ablb) || (!alb && !blb && ablb);
+}
+
+constexpr bool half_carry(std::uint8_t a, std::uint8_t b)
+{
+    return (((a & 0x0F) + (b & 0x0F)) & 0x10) == 0x10;
+}
+
+constexpr bool half_carry(std::uint16_t a, std::uint16_t b)
+{
+    return (((a & 0x0FFF) + (b & 0x0FFF)) & 0x1000) == 0x1000;
 }
