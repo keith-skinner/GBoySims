36cd2c1f1d8d7d959d2fadabe2c57c1acd8aee93
Keith Skinner <kskinns.dev@gmail.com>
2022-07-26 14:23:56 -0700

    Made a test more general to better show what's possible

--- test/LR35902/registers.cpp
+++ test/LR35902/registers.cpp
@@ -2,26 +2,28 @@
 #include <LR35902/registers.hpp>
 #include <LR35902/utility/bits.hpp>
 
+template<typename R8>
+void access_test(const typename R8::size_t value)
+{
+    using namespace LR35902;
+    using namespace LR35902::Register;
+    Registers regs;
+    regs.write(R8{}, value);
+    EXPECT_EQ(regs.read(R8{}), value);
+}
+
 GTEST_TEST(Registers, Register8Access)
 {
     using namespace LR35902;
-    Registers regs;
-    regs.write(A{}, A::size_t{1});
-    EXPECT_EQ(regs.read(A{}), 1);
-    regs.write(F{}, F::size_t{2});
-    EXPECT_EQ(regs.read(F{}), 2);
-    regs.write(B{}, B::size_t{3});
-    EXPECT_EQ(regs.read(B{}), 3);
-    regs.write(C{}, C::size_t{4});
-    EXPECT_EQ(regs.read(C{}), 4);
-    regs.write(D{}, D::size_t{5});
-    EXPECT_EQ(regs.read(D{}), 5);
-    regs.write(E{}, E::size_t{6});
-    EXPECT_EQ(regs.read(E{}), 6);
-    regs.write(H{}, H::size_t{7});
-    EXPECT_EQ(regs.read(H{}), 7);
-    regs.write(L{}, L::size_t{8});
-    EXPECT_EQ(regs.read(L{}), 8);
+    using namespace LR35902::Register;
+    access_test<A>(0x01);
+    access_test<F>(0x12);
+    access_test<B>(0x23);
+    access_test<C>(0x34);
+    access_test<D>(0x45);
+    access_test<E>(0x56);
+    access_test<H>(0x67);
+    access_test<L>(0x78);
 }
 
 template<typename R16, typename R8L, typename R8H>
@@ -53,10 +55,58 @@ void access_test()
 
 GTEST_TEST(Registers, Register16Access)
 {
-    using namespace LR35902;
-    Registers regs;
+    using namespace LR35902::Register;
     access_test<AF, A, F>();
     access_test<BC, B, C>();
     access_test<DE, D, E>();
     access_test<HL, H, L>();
+}
+
+GTEST_TEST(Registers, RegistersTypeTraits)
+{
+    using namespace LR35902::Register;
+    EXPECT_EQ(is_register_v<A>, true);
+    EXPECT_EQ(is_register_v<F>, true);
+    EXPECT_EQ(is_register_v<B>, true);
+    EXPECT_EQ(is_register_v<C>, true);
+    EXPECT_EQ(is_register_v<D>, true);
+    EXPECT_EQ(is_register_v<E>, true);
+    EXPECT_EQ(is_register_v<H>, true);
+    EXPECT_EQ(is_register_v<L>, true);
+    EXPECT_EQ(is_register_v<F>, true);
+    EXPECT_EQ(is_register_v<AF>, true);
+    EXPECT_EQ(is_register_v<BC>, true);
+    EXPECT_EQ(is_register_v<DE>, true);
+    EXPECT_EQ(is_register_v<HL>, true);
+    EXPECT_EQ(is_register_v<int>, false);
+
+    EXPECT_EQ(is_register8_v<A>, true);
+    EXPECT_EQ(is_register8_v<F>, true);
+    EXPECT_EQ(is_register8_v<B>, true);
+    EXPECT_EQ(is_register8_v<C>, true);
+    EXPECT_EQ(is_register8_v<D>, true);
+    EXPECT_EQ(is_register8_v<E>, true);
+    EXPECT_EQ(is_register8_v<H>, true);
+    EXPECT_EQ(is_register8_v<L>, true);
+    EXPECT_EQ(is_register8_v<F>, true);
+    EXPECT_EQ(is_register8_v<AF>, false);
+    EXPECT_EQ(is_register8_v<BC>, false);
+    EXPECT_EQ(is_register8_v<DE>, false);
+    EXPECT_EQ(is_register8_v<HL>, false);
+    EXPECT_EQ(is_register8_v<int>, false);
+
+    EXPECT_EQ(is_register16_v<A>, false);
+    EXPECT_EQ(is_register16_v<F>, false);
+    EXPECT_EQ(is_register16_v<B>, false);
+    EXPECT_EQ(is_register16_v<C>, false);
+    EXPECT_EQ(is_register16_v<D>, false);
+    EXPECT_EQ(is_register16_v<E>, false);
+    EXPECT_EQ(is_register16_v<H>, false);
+    EXPECT_EQ(is_register16_v<L>, false);
+    EXPECT_EQ(is_register16_v<F>, false);
+    EXPECT_EQ(is_register16_v<AF>, true);
+    EXPECT_EQ(is_register16_v<BC>, true);
+    EXPECT_EQ(is_register16_v<DE>, true);
+    EXPECT_EQ(is_register16_v<HL>, true);
+    EXPECT_EQ(is_register16_v<int>, false);
 }
