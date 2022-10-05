cdbeb57f6a55c30c2fa2eee9f735b901714cb556
Keith Skinner <kskinns.dev@gmail.com>
2022-10-04 23:57:52 -0700

    just gonna implement udlit here

--- src/defines.hpp
+++ src/defines.hpp
@@ -7,5 +7,5 @@ namespace GBOYSIM
     using data_t = uint8_t;
 }
 
-GBOYSIM::addr_t operator "" _a(unsigned long long int);
-GBOYSIM::data_t operator "" _d(unsigned long long int);
+consteval GBOYSIM::addr_t operator "" _a(unsigned long long int addr) { return addr; }
+consteval GBOYSIM::data_t operator "" _d(unsigned long long int data) { return data; }
