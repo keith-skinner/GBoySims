2dc51d2308e53244318bdc6dfc43e0ea5f34e3d3
Keith Skinner <kskinns.dev@gmail.com>
2022-11-11 11:09:38 -0800

    getting rid of annoying compiler warning

--- include/test/LR35902/arguments.cpp
+++ include/test/LR35902/arguments.cpp
@@ -5,4 +5,5 @@
 // Testing that nat16_t and sim16_t are explicitly convertable
 constexpr auto a = LR35902::nat16_t{5};
 constexpr auto b = static_cast<LR35902::sim16_t>(a);
+[[maybe_unused]] // proving it can be done
 constexpr auto c = static_cast<LR35902::nat16_t>(b);
