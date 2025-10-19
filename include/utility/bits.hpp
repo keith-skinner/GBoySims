a03c07a62532d63734a859e5de2e6d380171f5a0
Keith Skinner <kskinns.dev@gmail.com>
2025-10-18 20:57:17 -0700

    added move_bits utility function to really drive home where bits are being moved from and to. Probably should add something later that checks that the bits are contiguous and have the same count

--- include/utility/bits.hpp
+++ include/utility/bits.hpp
@@ -6,6 +6,7 @@
 #include <array>
 #include <algorithm>
 #include <concepts>
+#include <numeric>
 
 #include "uninitialized.hpp"
 #include "invalid.hpp"
@@ -13,7 +14,39 @@
 namespace utility
 {
 
+namespace impl {
+
+template<std::unsigned_integral U, std::size_t Left, std::size_t Right>
+[[nodiscard]]
+static 
+constexpr
+U move_bits(const U maskedValue) {
+    if constexpr (Left > Right) {
+        consteval std::size_t Shift = Left - Right;
+        return maskedValue << Shift;
+    } else if constexpr (Right > Left) {
+        consteval std::size_t Shift = Right - Left;
+        return maskedValue >> Shift;
+    } else {
+        return maskedValue;
+    }
+}
+
+} // namespace impl
+
+template<std::unsigned_integral U, U SelectMask, U MoveMask>
+[[nodiscard]]
+constexpr U move_bits(const U value) noexcept {
+    consteval std::size_t SHIFT_LEFT = std::countr_zero(MoveMask);
+    return impl::move_bits<U, , std::countr_zero(SelectMask)>(value & SelectMask);
+}
+
+using LargestInteger = unsigned long long;
+
+consteval std::array<LargestInteger, std::numeric_limits<LargestInteger>::
+
 template<std::unsigned_integral U>
+[[nodiscard]]
 constexpr bool half_carry(const U a, const U b)
 {
     static constexpr U mask = 1 << (std::numeric_limits<U>::digits/2);
@@ -21,6 +54,7 @@ constexpr bool half_carry(const U a, const U b)
 }
 
 template<std::unsigned_integral U>
+[[nodiscard]]
 constexpr bool carry(const U a, const U b)
 {
     static constexpr U mask = 1 << (std::numeric_limits<U>::digits - 1);
@@ -29,6 +63,7 @@ constexpr bool carry(const U a, const U b)
 }
 
 template<std::integral I>
+[[nodiscard]]
 constexpr bool half_carry(const I a, const I b)
 {
     using U = std::make_unsigned_t<I>;
@@ -36,50 +71,48 @@ constexpr bool half_carry(const I a, const I b)
 }
 
 template<std::integral I>
+[[nodiscard]]
 constexpr bool carry(const I a, const I b)
 {
     using U = std::make_unsigned_t<I>;
     return carry(static_cast<U>(a), static_cast<U>(b));
 }
 
+[[nodiscard]]
 constexpr uint8_t nibbleswap(const uint8_t value)
 {
-    return uint8_t{
-          ((value & 0x0F) << 4)
-        | ((value & 0xF0) >> 4)
-    };
+    return  move_bits<uint8_t, 0x0F, 0xF0>(value)
+        |   move_bits<uint8_t, 0xF0, 0x0F>(value)
+        ;
 }
 
 constexpr uint16_t byteswap(const uint16_t value)
 {
-    return uint16_t{
-          ( (value & 0x00'FF) << 010 )
-        | ( (value & 0xFF'00) >> 010 )
-    };
+    return  move_bits<uint16_t, 0x00'FF, 0xFF'00>(value)
+        |   move_bits<uint16_t, 0xFF'00, 0x00'FF>(value)
+        ;
 }
 
 constexpr uint32_t byteswap(const uint32_t value)
 {
-    return uint32_t{
-          ( (value & 0x00'00'00'FF) << 030 )
-        | ( (value & 0x00'00'FF'00) << 010 )
-        | ( (value & 0x00'FF'00'00) >> 010 )
-        | ( (value & 0xFF'00'00'00) >> 030 )
-    };
+    return  move_bits<uint32_t, 0x00'00'00'FF, 0xFF'00'00'00>(value)
+        |   move_bits<uint32_t, 0x00'00'FF'00, 0x00'FF'00'00>(value)
+        |   move_bits<uint32_t, 0x00'FF'00'00, 0x00'00'FF'00>(value)
+        |   move_bits<uint32_t, 0xFF'00'00'00, 0x00'00'00'FF>(value)
+        ;
 }
 
 constexpr uint64_t byteswap(const uint64_t value)
 {
-    return uint64_t{
-          ( (value & 0x00'00'00'00'00'00'00'FF) << 070 )
-        | ( (value & 0x00'00'00'00'00'00'FF'00) << 050 )
-        | ( (value & 0x00'00'00'00'00'FF'00'00) << 030 )
-        | ( (value & 0x00'00'00'00'FF'00'00'00) << 010 )
-        | ( (value & 0x00'00'00'FF'00'00'00'00) >> 010 )
-        | ( (value & 0x00'00'FF'00'00'00'00'00) >> 030 )
-        | ( (value & 0x00'FF'00'00'00'00'00'00) >> 050 )
-        | ( (value & 0xFF'00'00'00'00'00'00'00) >> 070 )
-    };
+    return  move_bits<uint64_t, 0x00'00'00'00'00'00'00'FF, 0xFF'00'00'00'00'00'00'00>(value)
+        |   move_bits<uint64_t, 0x00'00'00'00'00'00'FF'00, 0x00'FF'00'00'00'00'00'00>(value)
+        |   move_bits<uint64_t, 0x00'00'00'00'00'FF'00'00, 0x00'00'FF'00'00'00'00'00>(value)
+        |   move_bits<uint64_t, 0x00'00'00'00'FF'00'00'00, 0x00'00'00'FF'00'00'00'00>(value)
+        |   move_bits<uint64_t, 0x00'00'00'FF'00'00'00'00, 0x00'00'00'00'FF'00'00'00>(value)
+        |   move_bits<uint64_t, 0x00'00'FF'00'00'00'00'00, 0x00'00'00'00'00'FF'00'00>(value)
+        |   move_bits<uint64_t, 0x00'FF'00'00'00'00'00'00, 0x00'00'00'00'00'00'FF'00>(value)
+        |   move_bits<uint64_t, 0xFF'00'00'00'00'00'00'00, 0x00'00'00'00'00'00'00'FF>(value)
+        ;
 }
 
 template<typename T>
